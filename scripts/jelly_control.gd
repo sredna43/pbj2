23d0fa9dd741c7bd48adf4a5078a03e951073daf
Anders <sredna43@gmail.com>
2020-10-20 16:14:46 -0500

    initial commit

---
+++ scripts/jelly_control.gd
@@ -0,0 +1,14 @@
+# This will likely handle movement of the Jelly Character
+
+extends "res://scripts/player_constants.gd"
+
+var velocity = Vector2()
+var direction = Vector2()
+
+
+func _ready() -> void:
+	velocity.y = 1
+
+func _physics_process(_delta: float) -> void:
+	velocity.y += GRAVITY * _delta
+	velocity = .move_and_slide(velocity, Vector2.UP)
---
+++ scripts/player_constants.gd
@@ -0,0 +1,8 @@
+# Constants for players such as jump height, speed, etc
+
+extends KinematicBody2D
+
+class_name PlayerConstants
+
+export(float) var GRAVITY = 1000.0
+export(float) var PLAYER_SPEED = 500.0
