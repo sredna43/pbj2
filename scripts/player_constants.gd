48293838f8bed40fe3e996eed2bcf3f72e0b8432
Anders <sredna43@gmail.com>
2020-10-21 00:58:06 -0500

    Starting fresh

--- scripts/player_constants.gd
+++
@@ -1,8 +0,0 @@
-# Constants for players such as jump height, speed, etc
-
-extends KinematicBody2D
-
-class_name PlayerConstants
-
-export(float) var GRAVITY = 1000.0
-export(float) var PLAYER_SPEED = 500.0
