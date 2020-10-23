8268a21c6a999d1e562f9643d5fe54595fea9f1b
Anders Olson <olson16@stolaf.edu>
2020-10-22 21:20:00 -0500

    initial fsm commit for new fsm

---
+++ characters/shared/states/player_fsm.gd
@@ -0,0 +1,31 @@
+# Base Finite State Machine to be used by both players
+
+extends Node2D
+class_name PlayerFSM
+
+var states: Dictionary = {}
+var active_state: PlayerState
+var player: KinematicBody2D
+
+func init_states():
+    for state in get_children():
+        if state.tag:
+            states[state.tag] = state
+
+func init(p: KinematicBody2D):
+    player = p
+    init_states()
+    active_state = states.idle
+    active_state.enter(player)
+
+func run():
+    var next_state_tag = active_state.run(player)
+    if next_state_tag:
+        change_state(next_state_tag)
+
+func change_state(next_state_tag: String):
+    var next_state = states.get(next_state_tag)
+    if next_state:
+        active_state.exit(player)
+        active_state = next_state
+        active_state.enter(player)
---
+++ characters/shared/states/state.gd
@@ -0,0 +1,23 @@
+# Base class for all states to work from
+
+extends Node2D
+class_name PlayerState
+
+export var tag : String = ""
+export var player_to_control : String = ""
+
+func _ready():
+    tag = name.to_lower()
+
+# Player State
+
+func enter(player: KinematicBody2D):
+    player.play(tag)
+
+# warning-ignore:unused_argument
+func run(player: KinematicBody2D):
+    return null
+
+func exit(player: KinematicBody2D):
+    if player and player.anim:
+        player.anim.clear_queue()
