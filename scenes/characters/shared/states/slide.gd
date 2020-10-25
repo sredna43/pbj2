41a4f2deed062a596e5813bb23b535117950fb02
Anders Olson <olson16@stolaf.edu>
2020-10-25 09:39:24 -0500

    Motion is pretty much done, adding attack now

---
+++ scenes/characters/shared/states/slide.gd
@@ -0,0 +1,25 @@
+# Slide state, when the player is colliding with a wall and should slide
+
+extends PlayerState
+
+func run(player: KinematicBody2D, delta: float):
+    player.update_look_direction(player.get_wall_slide())
+    player.apply_gravity(player.gravity / player.slide_friction, delta)
+    player.move()
+    
+    # Hop off wall
+    if player.horizontal == player.get_wall_slide():
+        player.vx = player.run_speed * player.horizontal
+        player.move()
+        return "air"
+        
+    # Jump from wall
+    if player.jumping:
+        player.vx = player.run_speed * player.get_wall_slide()
+        return "jump"
+    
+    # Land on ground
+    if player.grounded:
+        return "run"
+    
+    return null
